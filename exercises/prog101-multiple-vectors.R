a04ceb4cc0a1c45a65c1d7acdc60681f7369cc00
Casey-Strattan <strattan@ucsb.edu>
2025-03-12 11:52:39 -0700

    edits made

--- exercises/prog101-multiple-vectors.R
+++ exercises/prog101-multiple-vectors.R
@@ -3,15 +3,23 @@
 instrument_deployed_hm <- c(730, 915, 1345)
 instrument_deployed_h <- floor(instrument_deployed_hm) / 100
 instrument_deployed_hdec <- (instrument_deployed_hm %% 100) / 60
-instrument_deployed <- instrument_deployed_h + instrument_deployed_hdec
+instrument_deployed_h <- floor(instrument_deployed_h) + instrument_deployed_hdec
+#The first expression puts the times into hr.min format
+#The second expression extracts the min from the time and finds its decimal equivalent (within the hour)
+#The third expression adds the hr and the decimal min
 
 # Based on the code above, calculate the duration of instrument deployments
 # using the instrument recovery times below. What units do the durations have?
 instrument_recovered_hm <- c(1600, 1920, 2015)
-???
-instrument_recovered <- ??? + ???
+instrument_recovered_h <- floor(instrument_recovered_hm) / 100
+instrument_recovered_mdec <- (instrument_recovered_hm %% 100) / 60
+instrument_recovered_h <- floor(instrument_recovered_h) + instrument_recovered_mdec
 
-instrument_durations <- ??? - ???
+deployment_duration_h <- instrument_recovered_h - instrument_deployed_h
+
+# Units of hours in decimal format
+
 
 # Which site had the longest duration? Use conditional indexing.
 site <- c("Santa Cruz", "Santa Rosa", "San Miguel")
+site_longest_dur <- site[which.max(deployment_duration_h)]
--- exercises/prog101-using-functions.R
+++ exercises/prog101-using-functions.R
@@ -1,13 +1,16 @@
 # Fill in the blanks so the answers are all 2. Use help for unfamiliar functions
 # if necessary.
-log10(????)
-????(4)
-ceiling(????.1)
+log10(100)
+sqrt(4)
+ceiling(1.1)
 
 # Find the functions for cosine and sine. What are the cosine and sine of 0?
 
 # What do you think the following functions do? Just go off the name, don't look
 # them up.
 read.csv()
+# Read a csv to the code
 boxplot()
+# Create a box plot from some data
 startsWith()
+# Check to see if a variable starts with something
